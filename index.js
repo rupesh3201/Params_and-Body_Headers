af686051d4a3650a3f1ad3b281a40af20b253a49
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-30 22:51:27 +0530

    added filter by price

--- index.js
+++ index.js
@@ -21,16 +21,15 @@ app.get('/fruits',(req,res)=>
 {
     const {price} = req.query
     const filtredfruits = fruits.filter((fruit)=>{
-        if(fruits.price==price)
+        if(fruit.price == price)
         {
-            return fruits
+            return fruit
         }
 
     })
     res.json({
         sucess:true,
-        data:fruits,
-        data2:filtredfruits, 
+        data:filtredfruits, 
         msg:"data Fetch sucessfully"
 
       
