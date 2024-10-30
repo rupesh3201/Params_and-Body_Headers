70c53cd65c663681d22c0f0d497284623c1867d5
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-30 18:28:25 +0530

    server created sucessfully

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import express from 'express';
+const app = express();
+
+app.get('/fruits',(req,res)=>
+{
+    res.json({
+        msg:"data send sucessfully"
+    })
+
+})
+const Port= 5000;
+app.listen(Port,()=>
+{
+    console.log(`server is running on http//${Port}`)
+})
